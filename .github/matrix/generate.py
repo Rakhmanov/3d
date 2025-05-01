87b6c7c2c8a27b9337efb5bbe2117a40077c3d01
Denis Shatilov <shatilov18@gmail.com>
2025-05-01 15:17:21 -0400

    Checking for files first

--- .github/matrix/generate.py
+++ .github/matrix/generate.py
@@ -7,14 +7,16 @@ changed_files = subprocess.check_output(
     ['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', 'HEAD']
 ).decode().splitlines()
 
-valid_folders = set()
+entries = []
 for path in changed_files:
     if path.endswith('.scad'):
-        parts = path.split('/')
-        if len(parts) > 1:
-            valid_folders.add(parts[0])
+        splitName = os.path.splitext(os.path.basename(path))
+        folder = os.path.dirname(path)
+        name = splitName[0]
+        extension = splitName[1]
+        entries.append({"folder": folder, "name": name, "extension": extension})
 
-matrix = {'folder': sorted(valid_folders)}
+matrix = {'include': sorted(entries)}
 print(f'matrix={json.dumps(matrix)}')
 with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
     f.write(f'matrix={json.dumps(matrix)}\n')
